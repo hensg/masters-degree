244ca9e70cdc9c6988876647981a8fa8eff762b4
Henrique Goulart <henriquedsg89@gmail.com>
2021-09-07 11:23:13 -0300

    Fix identation

--- emulab/bft-smart-parallel/mount_disks.sh
+++ emulab/bft-smart-parallel/mount_disks.sh
@@ -5,12 +5,12 @@ i=0
 
 for disk in disks
 do
-	sudo mkdir /checkpoint$i/ 
-	sudo chmod 777 /checkpoint$i/	
+    sudo mkdir /checkpoint$i/ 
+    sudo chmod 777 /checkpoint$i/	
     sudo mkfs.ext4 /dev/$disk
     sudo mount /dev/$disk /checkpoint$i/
-	sudo mkdir /checkpoint$i/states/
-	sudo mkdir /checkpoint$i/metadata/
-	sudo chmod 777 -R /checkpoint$i/
+    sudo mkdir /checkpoint$i/states/
+    sudo mkdir /checkpoint$i/metadata/
+    sudo chmod 777 -R /checkpoint$i/
     $i = $i + 1
 done

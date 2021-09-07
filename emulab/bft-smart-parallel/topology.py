d57e9d7e609a9614ff37671ea485f53034d4fc8a
Henrique Goulart <henriquedsg89@gmail.com>
2021-09-07 11:17:56 -0300

    Rewrite topology script with parametrized number of machines and types

--- emulab/bft-smart-parallel/topology.py
+++ emulab/bft-smart-parallel/topology.py
@@ -1,3 +1,13 @@
+"""A cluster node topology for testing bft-smart with parallel checkpointing.
+
+It uses the disk image 'urn:publicid:IDN+emulab.net+image+ScalableSMR//BFT-SMaRt-PCheckpointD430'
+that has the project source code in /srv/EMULAB-RND/
+
+Instructions:
+After machine starts, a script to mount the disks will run and you can start
+the bft-smart checkpointing experiments.
+"""
+
 # Import the Portal object.
 import geni.portal as portal
 # Import the ProtoGENI library.
@@ -10,46 +20,33 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
+request.setRoutingStyle('static')
 
-# Node node1
-node1 = request.RawPC('node1')
-node1.hardware_type = 'd430'
-node1.disk_image = "urn:publicid:IDN+emulab.net+image+ScalableSMR//CheckpointExperimentosD430";
+portal.context.defineParameter("n", "Number of machines", portal.ParameterType.INTEGER, 4)
+portal.context.defineParameter("t", "Type of machines", portal.ParameterType.STRING, 'd430')
 
-iface0 = node1.addInterface('eth0', pg.IPv4Address('10.1.1.2','255.255.255.0'))
+params = portal.context.bindParameters()
+if params.n < 1 or params.n > 36:
+    portal.context.reportError(portal.ParameterError("You must choose at least 1 and no more than 36 machines."))
 
-# Node node2
-node2 = request.RawPC('node2')
-node2.hardware_type = 'd430'
-node2.disk_image = "urn:publicid:IDN+emulab.net+image+ScalableSMR//CheckpointExperimentosD430";
-iface1 = node2.addInterface('eth0', pg.IPv4Address('10.1.1.3','255.255.255.0'))
+allowed_types = ["d430","d820"]
+if params.t not in allowed_types:
+    portal.context.reportError(portal.ParameterError("You must choose one of this types: " + ",".join(allowed_types)))
 
-# Node node3
-node3 = request.RawPC('node3')
-node3.hardware_type = 'd430'
-node3.disk_image = "urn:publicid:IDN+emulab.net+image+ScalableSMR//CheckpointExperimentosD430";
-iface2 = node3.addInterface('eth0', pg.IPv4Address('10.1.1.4','255.255.255.0'))
-
-# Node node4
-node4 = request.RawPC('node4')
-node4.hardware_type = 'd430'
-node4.disk_image = "urn:publicid:IDN+emulab.net+image+ScalableSMR//CheckpointExperimentosD430";
-iface3 = node4.addInterface('eth0', pg.IPv4Address('10.1.1.5','255.255.255.0'))
 
 # Link lan0
 link_lan0 = request.LAN('lan0')
 link_lan0.setNoBandwidthShaping()
 link_lan0.trivial_ok = True
-iface0.bandwidth = 10000000
 link_lan0.addInterface(iface0)
-iface1.bandwidth = 10000000
-link_lan0.addInterface(iface1)
-iface2.bandwidth = 10000000
-link_lan0.addInterface(iface2)
-iface3.bandwidth = 10000000
-link_lan0.addInterface(iface3)
 
-request.setRoutingStyle('static')
+for i in range(params.n):
+    node = request.RawPC('Node'+(i+1))
+    node.hardware_type = params.t
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+ScalableSMR//BFT-SMaRt-PCheckpointD430";
+    iface = node.addInterface('eth0', pg.IPv4Address('10.1.1.'+(i+2),'255.255.255.0'))
+    iface.bandwidth = 10000000
+    link_lan0.addInterface(iface)
 
 # Print the generated rspec
 pc.printRequestRSpec(request)
